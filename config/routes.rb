e4eb48f33f7cc1fcd0ff908e4b0d65d5093975a1
Laetitia Massa <laetitiamassa@gmail.com>
2016-03-12 08:50:48 +0100

    added devise logic for mobile - still to fix some issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :happenings
   root to: 'visitors#index'
-  devise_for :users
+  
+  devise_for :users, :controllers => {sessions: 'user/sessions', registrations: 'user/registrations', passwords: 'user/passwords' }
   resources :users
 end
